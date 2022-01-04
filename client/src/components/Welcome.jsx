793843128859e99167f569d53d4f2b3f23815c74
Vjet Node JS <51021128+huuhoa2309@users.noreply.github.com>
2022-01-04 09:42:54 +0700

    config Tailwindcss.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,7 +4,8 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "./";
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
     <input
@@ -16,15 +17,13 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
         className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
     />
 );
-
 const Welcome = () => {
-
     const connectWallet = () => {
         alert("Connect Wallet");
     }
-    const handleSubmit = () => { }
 
-
+    const handleSubmit = (e) => {
+    };
 
     return (
         <div className="flex w-full justify-center items-center">
@@ -36,77 +35,79 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Khám phá thế giới tiền điện tử. Mua và bán tiền điện tử dễ dàng trên Vrypto.
                     </p>
-                    <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                    <button
+                        type="button"
+                        onClick={connectWallet}
+                        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                    >
+                        <AiFillPlayCircle className="text-white mr-2" />
                         <p className="text-white text-base font-semibold">
                             Kết nối đến ví của bạn
                         </p>
                     </button>
 
 
-
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-                        <div className={`rounded-tl-2xl ${commonStyles}`}>
+                        <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
                             Độ tin cậy
                         </div>
-                        <div className={commonStyles}>
-                            Bảo mật
-                        </div>
-                        <div className={`sm:rounded-tr-2xl ${commonStyles}`}>
+                        <div className={companyCommonStyles}>Bảo mật</div>
+                        <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
                             Ethereum
                         </div>
-                        <div className={`sm:rounded-bl-2xl ${commonStyles}`}>
+                        <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
                             Web 3.0
                         </div>
-                        <div className={commonStyles}>
-                            Chi phí thấp
-                        </div>
-                        <div className={`rounded-br-2xl ${commonStyles}`}>
+                        <div className={companyCommonStyles}>Chi phí thấp</div>
+                        <div className={`rounded-br-2xl ${companyCommonStyles}`}>
                             Blockchain
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex flex-col flex-1 items-center justify-center w-full">
-                <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
-                    <div className="flex justify-between flex-col w-full h-full">
-                        <div className="flex justify-between items-start">
-                            <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                                <SiEthereum fontSize={21} color="#fff" title="Ethereum" />
+
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+                    <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                    <SiEthereum fontSize={21} color="#fff" title="Ethereum" />
+                                </div>
+                                <BsInfoCircle fontSize={17} color="#fff" />
                             </div>
-                            <BsInfoCircle fontSize={17} color="#fff" />
-                        </div>
-                        <div className="">
-                            <p className="text-white font-light text-sm">
-                                Vjetgolang
-                            </p>
-                            <p className="text-white font-semibold text-lg mt-1">
-                                Ethereum
-                            </p>
+                            <div>
+                                <p className="text-white font-light text-sm">
+                                    Vjetgolang
+                                </p>
+                                <p className="text-white font-semibold text-lg mt-1">
+                                    Ethereum
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                    <Input placeholder="Địa chỉ đến" name="addressTo" type="text" handleChange={() => { }} />
-                    <Input placeholder="Giá (ETH)" name="amount" type="number" handleChange={() => { }} />
-                    <Input placeholder="Từ khóa (Gif)" name="keyword" type="text" handleChange={() => { }} />
-                    <Input placeholder="Tin nhắn" name="message" type="text" handleChange={() => { }} />
-                    <div className="h-[1px] w-full bg-gray-400 my-2" />
-                    {false
-                        ? <Loader />
-                        : (
-                            <button
-                                type="button"
-                                onClick={() => { }}
-                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
-                            >
-                                Gửi ngay
-                            </button>
-                        )}
-                </div>
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                        <Input placeholder="Địa chỉ đến" name="addressTo" type="text" handleChange={() => { }} />
+                        <Input placeholder="Giá (ETH)" name="amount" type="number" handleChange={() => { }} />
+                        <Input placeholder="Từ khóa (Gif)" name="keyword" type="text" handleChange={() => { }} />
+                        <Input placeholder="Tin nhắn" name="message" type="text" handleChange={() => { }} />
 
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+                        {false
+                            ? <Loader />
+                            : (
+                                <button
+                                    type="button"
+                                    onClick={handleSubmit}
+                                    className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                                >
+                                    Gửi ngay
+                                </button>
+                            )}
+                    </div>
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default Welcome;
